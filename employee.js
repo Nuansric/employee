7609ffe8336ee1905100a2896eebc233ce107df4
JimmyJ <jimmyj.inbox@gmail.com>
2016-12-01 21:40:28 -0600

    latest slacked version

--- employee.js
+++ employee.js
@@ -14,25 +14,24 @@ $(document).ready(function(){
 	var employeeName = "";
 	var employeeRole = "";
 	var startDate = "";
-	var monthsWorked = 0;
+	var monthsWorked =0;
 	var monthlyRate = 0;
 	var totalBill = 0;
+	var convertedDate = moment(new Date(startDate));
+
 
 $("#submit").on("click", function(){
 
 	employeeName = $("#employee-name").val().trim();
 	employeeRole = $("#role").val().trim();
 	startDate = $("#start-date").val().trim();
+	
 	//monthsWorked = parseInt($("#months-worked").val().trim());
 	monthlyRate = parseInt($("#rate").val().trim());
+
 	//totalBill = monthsWorked * monthlyRate;
 
-	console.log(employeeName);
-	console.log(employeeRole);
-	console.log(startDate);
-	console.log(monthsWorked);
-	console.log(monthlyRate);
-	console.log(totalBill);
+	
 
 	//saving to database
 
@@ -54,28 +53,40 @@ $("#submit").on("click", function(){
 
 database.ref().on("child_added", function(snapshot){
 
+var convertedDate = moment(new Date(snapshot.val().start));
+	monthsWorked = moment().diff(moment(convertedDate), "months");
+	totalBill = monthsWorked * snapshot.val().rate;
+	
 	console.log(snapshot.val());
 	console.log(snapshot.val().name);
 	console.log(snapshot.val().role);
 	console.log(snapshot.val().start);
-	console.log(snapshot.val().worked);
+	//console.log(snapshot.val().worked);
 	console.log(snapshot.val().rate);
-	console.log(snapshot.val().bill);
+	//console.log(snapshot.val().bill);
+	console.log(employeeName);
+	console.log(employeeRole);
+	console.log(startDate);
+	console.log("converted"+convertedDate);
+	console.log("months"+monthsWorked);
+	console.log(monthlyRate);
+	console.log(totalBill);
 
 	//calculated variable
-	monthsWorked = (snapshot.val().dateAdded) - startDate;
-	totalBill = monthsWorked * monthlyRate;
+	//monthsWorked = (snapshot.val().dateAdded) - startDate;
+	
+	
 
 
 	//change HTML
 
-	$("#display").append("<div class = 'employeeDisplay'>"+
-		"<span id='displayName'>"+ snapshot.val().name + 
-		"</span><span id='displayRole'>" + snapshot.val().role +
-		"</span><span id='displayDate'>" + snapshot.val().start + 
-		"</span><span id='displayMonths'>" + monthsWorked + 
-		"</span><span id='displayRate'>" + snapshot.val().rate + 
-		"</span><span id='displayBill'>" + totalBill + "</span></div>");
+	$("#display").append("<tr class = 'employeeDisplay'>"+
+		"<td id='displayName'>"+ snapshot.val().name + 
+		"</td><td id='displayRole'>" + snapshot.val().role +
+		"</td><td id='displayDate'>" + snapshot.val().start + 
+		"</td><td id='displayMonths'>" + monthsWorked + 
+		"</td><td id='displayRate'>" + "$"+ snapshot.val().rate + 
+		"</td><td id='displayBill'>" + "$"+ totalBill + "</td></tr>");
 
 }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
